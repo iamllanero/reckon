e08b23c1b81fd64f631ba747ce67648846c26e51
crouguer <crouguer@pm.me>
2023-01-09 20:44:35 -0500

    load tags in config, and allow for local tag definitions and overrides as tags_local.toml

--- config.py
+++ config.py
@@ -3,3 +3,11 @@ from reckon.constants import *
 
 with open(TXNS_TOML, 'rb') as f:
     TXNS_CONFIG = tomllib.load(f)
+
+with open(TAGS_FILE, 'rb') as f:
+    TAGS = tomllib.load(f)
+
+    with open(TAGS_LOCAL_FILE, 'rb') as g:
+        local_tags = tomllib.load(g)
+
+    TAGS.update(local_tags)
