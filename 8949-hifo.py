8ff007a146c91f0dc135a4563e08dd5b0f5a1c25
Llanero <iamllanero@gmail.com>
2023-01-12 12:41:42 -0600

    Update 8949 to calc for all tokens in priced.csv

--- 8949-hifo.py
+++ 8949-hifo.py
@@ -1,6 +1,74 @@
 import pandas as pd
 from reckon.utils import list_to_csv
 
+def calc_sell(df, symbol):
+    """
+    Calculates for every sale, the matching buy/income lot(s).
+
+    Returns:
+        List - Each item is a list representing a sell/buy lot
+    """
+
+    if symbol == None:
+        return
+
+    # Get a clean dataframe (scope down to single token and taxable tx types)
+    df = df.query("symbol == @symbol and txn_type in ['buy', 'sell', 'income']")\
+        .copy()\
+        .sort_values(by=['date'])\
+        .reset_index(drop=True)
+
+    # Augment with additional columns
+    df['qty_unsold'] = df['qty'].where(df['txn_type'] != 'sell')
+    df['unit_value'] = df['usd_value'] / df['qty']
+
+    # Create list for tracking the tokens sold
+    sell_list = []
+
+    # Start looking through the transaction dataframe
+    for i, row in df.iterrows():
+        if row['txn_type'] == 'sell':
+            qty_to_sell = row['qty']
+            while qty_to_sell > 0:
+                # Check that there are any buys
+                if len(df.iloc[0:i].query("txn_type in ['buy', 'income'] and qty_unsold > 0")) == 0:
+                    print(f"WARN: No buy/income found for {row['qty']} {row['symbol']} sold on {row['date']}")
+                    return sell_list
+                # Find the highest cost basis in transactions prior to current
+                hi_row = (df.iloc[0:i].query("txn_type in ['buy', 'income'] and qty_unsold > 0")
+                    )['unit_value'].idxmax()
+                hi_unsold = df.iloc[hi_row]['qty_unsold']
+                cost_basis = 0
+                proceeds = 0
+                qty_sold = 0
+                if qty_to_sell <= hi_unsold:
+                    # Sold everything against one buy
+                    qty_sold = qty_to_sell
+                    df.iloc[hi_row, df.columns.get_loc('qty_unsold')] -= qty_to_sell
+                    qty_to_sell = 0
+                    cost_basis = df.iloc[hi_row]['usd_value']
+                    proceeds = row['usd_value']
+                else:
+                    # Sold only partial against a buy; need to keep going
+                    qty_sold = df.iloc[hi_row]['qty_unsold']
+                    qty_to_sell -= df.iloc[hi_row]['qty_unsold']
+                    df.iloc[hi_row, df.columns.get_loc('qty_unsold')] = 0
+                    cost_basis = hi_unsold * df.iloc[hi_row]['unit_value']
+                    proceeds = hi_unsold * row['unit_value']
+                sell_list.append([
+                    symbol,
+                    qty_sold,
+                    row['date'],
+                    df.iloc[hi_row]['date'],
+                    proceeds,
+                    cost_basis,
+                    proceeds - cost_basis,
+                    row['unit_value'],
+                    df.iloc[hi_row]['unit_value'],
+                ])
+
+    return sell_list
+
 def main():
     """
     Generate a csv with the following columns:
@@ -30,21 +98,6 @@ def main():
         'usd_value': 'float64',
     })
 
-    symbol = 'ETH'
-
-    # Get a clean dataframe (scope down to single token and taxable tx types)
-    df = df.query("symbol == @symbol and txn_type in ['buy', 'sell', 'income']")\
-        .copy()\
-        .sort_values(by=['date'])\
-        .reset_index(drop=True)
-
-    # Augment with additional columns
-# ?   df['qty_unsold'] = df['qty']
-    df['qty_unsold'] = df['qty'].where(df['txn_type'] != 'sell')
-    df['unit_value'] = df['usd_value'] / df['qty']
-
-    df.to_csv('data/8949-hifo-intermediate.csv')
-
     # Create list for tracking the tokens sold
     sell_list = [[
         'symbol',
@@ -58,52 +111,13 @@ def main():
         'cost unit',
         ]]
 
-    # Start looking through the transaction dataframe
-    for i, row in df.iterrows():
-        if row['txn_type'] == 'sell':
-            qty_to_sell = row['qty']
-            while qty_to_sell > 0:
-                # Find the highest cost basis in transactions prior to current
-                # hi_row = df[
-                #     ((df['txn_type'] == 'buy') | (df['txn_type'] == 'income')) \
-                #         & (df['qty_unsold'] > 0)]['unit_value']\
-                #             .iloc[0:i-1].idxmax()
-                hi_row = (df.iloc[0:i].query("txn_type in ['buy', 'income'] and qty_unsold > 0")
-                    )['unit_value'].idxmax()
-                print(f"Sold on line {i}. Looking up to {hi_row}")
-                hi_unsold = df.iloc[hi_row]['qty_unsold']
-                cost_basis = 0
-                proceeds = 0
-                qty_sold = 0
-                if qty_to_sell <= hi_unsold:
-                    print(f"    Fully sold against on line {hi_row}. Remaining = {qty_to_sell}")
-                    # Sold everything against one buy
-                    qty_sold = qty_to_sell
-                    df.iloc[hi_row, df.columns.get_loc('qty_unsold')] -= qty_to_sell
-                    qty_to_sell = 0
-                    cost_basis = df.iloc[hi_row]['usd_value']
-                    proceeds = row['usd_value']
-                else:
-                    print(f"    Partially sold against on line {hi_row}. Remaining = {qty_to_sell}")
-                    # Sold only partial against a buy; need to keep going
-                    qty_sold = df.iloc[hi_row]['qty_unsold']
-                    qty_to_sell -= df.iloc[hi_row]['qty_unsold']
-                    df.iloc[hi_row, df.columns.get_loc('qty_unsold')] = 0
-                    cost_basis = hi_unsold * df.iloc[hi_row]['unit_value']
-                    proceeds = hi_unsold * row['unit_value']
-                sell_list.append([
-                    symbol,
-                    qty_sold,
-                    row['date'],
-                    df.iloc[hi_row]['date'],
-                    proceeds,
-                    cost_basis,
-                    proceeds - cost_basis,
-                    row['unit_value'],
-                    df.iloc[hi_row]['unit_value'],
-                ])
+    symbols = df['symbol'].unique()
 
-    df.to_csv('data/8949-hifo-intermediate-2.csv')
+    for symbol in symbols:
+        print(f"INFO: Calculating symbol {symbol}")
+        sell_list.extend(calc_sell(df, symbol))
+
+    # df.to_csv('data/8949-hifo-wip.csv')
     list_to_csv(sell_list, 'data/8949-hifo.csv')
 
 if __name__ == '__main__':
