30b3f693dcc302a10e079b2ce1ad75ff29e97468
Llanero <iamllanero@gmail.com>
2023-09-27 08:49:46 +0100

    Remove deps on utils

--- reckon/coingecko/coingecko.py
+++ reckon/coingecko/coingecko.py
@@ -1,19 +1,21 @@
+import csv
 import datetime
 import json
 import os
 import requests
 import time
-import utils
+# import utils
 from config import (
-    COINGECKO_ID_EXPLICIT, 
-    PRICE_CACHE_OUTPUT, 
-    PRICE_MANUAL_OUTPUT, 
-    PRICE_MISSING_OUTPUT, 
+    COINGECKO_ID_EXPLICIT,
+    PRICE_CACHE_OUTPUT,
+    PRICE_MANUAL_FILE,
+    PRICE_MISSING_OUTPUT,
     COINGECKO_COINS_LIST_FILE
-    )
+)
 
 COINGECKO_ID_LIST = json.load(open(COINGECKO_COINS_LIST_FILE))
 COINGECKO_IDS = [elem['symbol'] for elem in COINGECKO_ID_LIST]
+
 
 def get_coin_id(symbol):
     """
@@ -26,7 +28,7 @@ def get_coin_id(symbol):
 
     Parameters:
         symbol (str): ERC 20 token symbol
-    
+
     Returns:
         string: CG coin ID
 
@@ -35,7 +37,8 @@ def get_coin_id(symbol):
         # print(f'PRELIST {date} | {purchase_token} => {COINGECKO_ID_DICT[purchase_token]}')
         return COINGECKO_ID_EXPLICIT[symbol.lower()]
     elif symbol.lower() in COINGECKO_IDS:
-        matches = [x['id'] for x in COINGECKO_ID_LIST if x['symbol'] == symbol.lower()]
+        matches = [x['id']
+                   for x in COINGECKO_ID_LIST if x['symbol'] == symbol.lower()]
         if len(matches) == 1:
             # print(f'LIST {date} | {purchase_token} => {matches[0]}')
             COINGECKO_ID_EXPLICIT[symbol.lower()] = matches[0]
@@ -44,10 +47,12 @@ def get_coin_id(symbol):
             print(f'ERROR: MORE THAN 1 COIN ID FOR {symbol} => {matches}')
     else:
         print(f'ERROR: NO COIN ID FOUND FOR {symbol}')
-        print(f'  You can manually set an entry for {symbol} in reckon.constants.COINGECKO_ID_EXPLICIT')
+        print(
+            f'  You can manually set an entry for {symbol} in reckon.constants.COINGECKO_ID_EXPLICIT')
         print(f'  "{symbol}": "<<VALID COIN ID>>",')
     # print(f'Lookup {coin_id}')
     return None
+
 
 def get_cached_historical_price(symbol: str, date: datetime):
     """
@@ -68,13 +73,13 @@ def get_cached_historical_price(symbol: str, date: datetime):
     if type(date) != datetime.datetime:
         raise Exception("Date is not a valid datetime object")
 
-    if os.path.isfile(PRICE_MANUAL_OUTPUT):
-        with open(PRICE_MANUAL_OUTPUT, 'r') as f:
+    if os.path.isfile(PRICE_MANUAL_FILE):
+        with open(PRICE_MANUAL_FILE, 'r') as f:
             for line in f:
                 ldate, lsymbol, lprice, ldate_added, lsource = line.rstrip().split(',')
                 if ldate == date.strftime("%Y-%m-%d") and \
-                    lsymbol.lower() == symbol.lower() and \
-                    lprice != '':
+                        lsymbol.lower() == symbol.lower() and \
+                        lprice != '':
                     return float(lprice)
 
     if os.path.isfile(PRICE_CACHE_OUTPUT):
@@ -82,17 +87,17 @@ def get_cached_historical_price(symbol: str, date: datetime):
             for line in f:
                 ldate, lsymbol, lprice, ldate_added, lsource = line.rstrip().split(',')
                 if ldate == date.strftime("%Y-%m-%d") and \
-                    lsymbol.lower() == symbol.lower() and \
-                    lprice != '':
+                        lsymbol.lower() == symbol.lower() and \
+                        lprice != '':
                     # print(f'Cache hit for {date.strftime("%Y-%m-%d")}:{symbol}:{price}')
                     return float(lprice)
-                    
+
     return None
 
 
-def save_historical_price(symbol: str, 
-                          date: datetime, 
-                          price: float, 
+def save_historical_price(symbol: str,
+                          date: datetime,
+                          price: float,
                           source: str = "Coingecko"):
     """
     Saves the price data to PRICE_CACHE_FILE if it doesn't already exist.
@@ -108,7 +113,8 @@ def save_historical_price(symbol: str,
     if cached_price == None:
         with open(PRICE_CACHE_OUTPUT, 'a') as f:
             date_added = datetime.datetime.now().strftime('%Y-%m-%d')
-            f.write(f"{date.strftime('%Y-%m-%d')},{symbol},{price},{date_added},{source}\n")
+            f.write(
+                f"{date.strftime('%Y-%m-%d')},{symbol},{price},{date_added},{source}\n")
     else:
         # print(f'WARN: Entry for {date}|{symbol}|{price} already exists')
         pass
@@ -122,7 +128,7 @@ def save_missing_price(symbol: str, date: datetime):
         symbol (str): ERC-20 token symbol
         date (datetime): Date of the price
     """
-    print(f"Saving {symbol} price for {date} to {PRICE_MISSING_OUTPUT}")
+    # print(f"Saving {symbol} price for {date} to {PRICE_MISSING_OUTPUT}")
     with open(PRICE_MISSING_OUTPUT, 'a') as f:
         date_added = datetime.datetime.now().strftime('%Y-%m-%d')
         f.write(f"{date.strftime('%Y-%m-%d')},{symbol},?,{date_added},Missing\n")
@@ -166,18 +172,21 @@ def get_historical_price(symbol: str, date: datetime):
 
     fetched = False
     while not fetched:
-        print(f"Fetching /{coin_id}/history?date={date.strftime('%d-%m-%Y')}", end=" => ")
+        print(
+            f"Fetching /{coin_id}/history?date={date.strftime('%d-%m-%Y')}", end=" => ")
         response = requests.get(url)
         if response.status_code == 200:
             fetched = True
-            print(f"200 OK")
             try:
-                price = float(utils.get_nested_dict(response.json(), 'market_data.current_price.usd'))
+                # price = float(utils.get_nested_dict(
+                #     response.json(), 'market_data.current_price.usd'))
+                price = response.json['market_data']['current_price']['usd']
                 save_historical_price(symbol, date, price)
+                print(f"200 OK")
                 time.sleep(3)
                 return price
             except:
-                print(f"ERROR: No price for {date} | {symbol}")
+                print(f"200 OK / ERROR: No price for {date} | {symbol}")
                 save_missing_price(symbol, date)
                 time.sleep(3)
                 return None
@@ -197,25 +206,30 @@ def get_historical_price(symbol: str, date: datetime):
             return None
 
 
-def clean_cache():
+def clean_cache(file_path):
     """
     Maintenance function to clean up the PRICE_CACHE_FILE.
 
     Cleans cache by: 1) Removing duplicates, 2) Sorting
     """
     cache = []
-    with open(PRICE_CACHE_OUTPUT, 'r') as f:
+    with open(file_path, 'r') as f:
         for line in f:
             date, symbol, price, date_added, source = line.rstrip().split(',')
-            if [date,symbol] not in cache:
-                cache.append([date,symbol,price,date_added,source])
+            if [date, symbol] not in cache:
+                cache.append([date, symbol, price, date_added, source])
             else:
-                print(f'ERROR: Discarding duplicate cache result for {date}|{symbol}|{price}|{source}')
+                print(
+                    f'ERROR: {file_path}: Discarding duplicate cache result for {date}|{symbol}|{price}|{source}')
     cache.sort()
-    utils.list_to_csv(cache, PRICE_CACHE_OUTPUT)
+    # utils.list_to_csv(cache, file_path)
+    with open(file_path, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerows(cache)
 
 
-clean_cache()
+clean_cache(PRICE_CACHE_OUTPUT)
+clean_cache(PRICE_MISSING_OUTPUT)
 
 if __name__ == '__main__':
     # save_historical_price('btc', datetime.datetime(2021, 2, 7), 100.0)
